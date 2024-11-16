387c21b226bfd2cfdb93a1226a3c5775580eb3b7
Olesya <olesya.kiril2014@gmail.com>
2024-11-16 13:56:53 +0700

    remov unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,3 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-
 import './style_title_about_add_button/stylesMain.scss';
 import './presentation/addition_task/addition_task.scss';
 import "./presentation/creat_task/create_tasks.scss";
